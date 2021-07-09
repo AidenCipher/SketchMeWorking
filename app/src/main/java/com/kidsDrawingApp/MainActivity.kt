df6ecb57dedf1a2e0325bd70f0db507b6d545cae
MSAbhishek214 <abhishek.rotti755@gmail.com>
2021-07-09 18:33:51 +0530

    130 08-07-21 Add share feature, add custom progress bar. Saved image doesn't show in gallery. Need to fix bug

--- app/src/main/java/com/kidsDrawingApp/MainActivity.kt
+++ app/src/main/java/com/kidsDrawingApp/MainActivity.kt
@@ -7,6 +7,8 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
+import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -18,6 +20,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
@@ -66,6 +71,14 @@ class MainActivity : AppCompatActivity() {
 
         ib_undo.setOnClickListener{
             drawing_view.onClickUndo()
+        }
+
+        ib_save.setOnClickListener{
+            if(isReadStorageAllowed()){
+                BitMapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
+            }else{
+                requestStoragePermission()
+            }
         }
 
     }
@@ -183,6 +196,48 @@ class MainActivity : AppCompatActivity() {
         view.draw(canvas)
 
         return returnBitMap
+    }
+
+    private inner class BitMapAsyncTask(val mBitMap: Bitmap): AsyncTask<Any, Void, String>() {
+        override fun doInBackground(vararg p0: Any?): String {
+            var result = ""
+            if(mBitMap != null){
+                try {
+
+                    val bytes = ByteArrayOutputStream()
+                    mBitMap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    val f = File(externalCacheDir!!.absoluteFile.toString() + File.separator  + "SketchMe_" + System.currentTimeMillis() / 1000 + ".png")
+                    val fos = FileOutputStream(f)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result = f.absolutePath
+
+                }catch (e: Exception){
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if(!result!!.isEmpty()){
+                Toast.makeText(this@MainActivity, "Image saved successfully!: $result", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this@MainActivity, "Image not saved. Please try again: $result", Toast.LENGTH_SHORT).show()
+            }
+
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result), null){
+                path, uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+                shareIntent.type = "image/png"
+                startActivity(Intent.createChooser(shareIntent, "Share"))
+            }
+
+        }
+
     }
 
     companion object{
